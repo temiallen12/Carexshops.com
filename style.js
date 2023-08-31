53396b0effa9592e3dc04bc03837378fd91849cb
temiallen <zephtemi@gmail.com>
2023-08-31 11:14:36 +0100

    neew

--- style.js
+++ style.js
@@ -1,9 +1,9 @@
-var myIndex = 0;
+let myIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slide4");
+  let i;
+  let x = document.getElementsByClassName("slide4");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
